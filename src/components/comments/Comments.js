33963309e312d02a5feac1672d003da90ee19619
emir karatas <emirkaratassss@gmail.com>
2023-04-21 23:54:00 +0300

    Comments

--- src/components/comments/Comments.js
+++ src/components/comments/Comments.js
@@ -1,8 +1,49 @@
 import React from 'react'
+import "./comments.scss"
 
-function Comments({post}) {
+function Comments({ post }) {
   return (
-    <div>{post.postId}</div>
+    <div className='comments-modal'><hr />
+      <div className="row">
+        <div className="user-info d-flex align-items-start mb-2 col-12">
+          <img src="https://cdn-icons-png.flaticon.com/512/3135/3135715.png" alt="" />
+          <div className="card relevant-comment ms-2 p-1">
+            <span className='ms-2 fw-bold'>Emir Karataş</span>
+            <div className='ms-2'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Culpa perferendis nemo sunt aliquid voluptate cum qui, natus quisquam laborum ad?</div>
+          </div>
+        </div><div className="user-info d-flex align-items-start mb-2 col-12">
+          <img src="https://cdn-icons-png.flaticon.com/512/3135/3135715.png" alt="" />
+          <div className="card relevant-comment ms-2 p-1">
+            <span className='ms-2 fw-bold'>Emir Karataş</span>
+            <div className='ms-2'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Culpa perferendis nemo sunt aliquid voluptate cum qui, natus quisquam laborum ad?</div>
+          </div>
+        </div><div className="user-info d-flex align-items-start mb-2 col-12">
+          <img src="https://cdn-icons-png.flaticon.com/512/3135/3135715.png" alt="" />
+          <div className="card relevant-comment ms-2 p-1">
+            <span className='ms-2 fw-bold'>Emir Karataş</span>
+            <div className='ms-2'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Culpa perferendis nemo sunt aliquid voluptate cum qui, natus quisquam laborum ad?</div>
+          </div>
+        </div><div className="user-info d-flex align-items-start mb-2 col-12">
+          <img src="https://cdn-icons-png.flaticon.com/512/3135/3135715.png" alt="" />
+          <div className="card relevant-comment ms-2 p-1">
+            <span className='ms-2 fw-bold'>Emir Karataş</span>
+            <div className='ms-2'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Culpa perferendis nemo sunt aliquid voluptate cum qui, natus quisquam laborum ad?</div>
+          </div>
+        </div><div className="user-info d-flex align-items-start mb-2 col-12">
+          <img src="https://cdn-icons-png.flaticon.com/512/3135/3135715.png" alt="" />
+          <div className="card relevant-comment ms-2 p-1">
+            <span className='ms-2 fw-bold'>Emir Karataş</span>
+            <div className='ms-2'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Culpa perferendis nemo sunt aliquid voluptate cum qui, natus quisquam laborum ad?</div>
+          </div>
+        </div><div className="user-info d-flex align-items-start mb-2 col-12">
+          <img src="https://cdn-icons-png.flaticon.com/512/3135/3135715.png" alt="" />
+          <div className="card relevant-comment ms-2 p-1">
+            <span className='ms-2 fw-bold'>Emir Karataş</span>
+            <div className='ms-2'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Culpa perferendis nemo sunt aliquid voluptate cum qui, natus quisquam laborum ad?</div>
+          </div>
+        </div>
+      </div>
+    </div>
   )
 }
 
--- src/components/post/Post.js
+++ src/components/post/Post.js
@@ -16,6 +16,7 @@ import { Form } from 'react-bootstrap'
 
 
 function Post({ post }) {
+    const darkMode = localStorage.getItem("darkMode")
     const formik = useFormik({
         initialValues: {
             comment: "",
@@ -66,7 +67,7 @@ function Post({ post }) {
                 <p className="card-text">{post.description}</p>
             </div>
             {
-                !isImageEmpty ? <><Swiper
+                !isImageEmpty && <Swiper
                     pagination={{
                         clickable: true,
                         dynamicBullets: true,
@@ -77,106 +78,67 @@ function Post({ post }) {
                 >
                     {post.image.map((image, index) => (<SwiperSlide key={post.postId * post.userId + index}><img src={image} className="card-img-top" /></SwiperSlide>))}
                 </Swiper>
-                    <div className="card-footer">
-                        <div className="d-flex justify-content-between align-items-center">
-                            <div className="footer-icons h3">
-                                {
-                                    like ? <FcLike onClick={handleLike} /> : <AiOutlineHeart onClick={handleLike} />
-                                }
-                                <FiShare className='ms-2' />
-                            </div>
-                            <span className='comments' onClick={showCommentModal}><span className='fw-bold'>117</span> Yorum</span>
-                        </div>
-                        <span className='like-count' onClick={showLikeModal}><span className='fw-bold'>15</span> beğenme</span>
-                        <Modal title="Beğenmeler" open={isLikeModalOpen} onCancel={handleLikeCancel} footer={null} className='custom-modal' centered>
-                            <Likes post={post} />
-                        </Modal>
-                        <Modal title="Yorumlar" open={isCommentModalOpen} onCancel={handleCommentCancel} footer={null} className='custom-modal' centered>
-                            <Comments post={post} />
-                        </Modal>
-                        <Form onSubmit={formik.handleSubmit} className='d-flex align-items-center comment-form'>
-                            <Form.Group className="mb-3 mt-3 w-100">
-                                <Form.Control
-                                    name='comment'
-                                    className='form-control me-2'
-                                    as="textarea"
-                                    placeholder="Yorum Yaz"
-                                    onChange={formik.handleChange}
-                                />
-                            </Form.Group>
-                            <button className="btn btn-outline-dark ms-2 h-100" type="submit">Gönder</button>
-                        </Form>
-                        <div className="user-info d-flex align-items-start mb-2">
-                            <div className="avatar position-relative">
-                                <img src="https://cdn-icons-png.flaticon.com/512/3135/3135715.png" alt="" />
-                                <span className="status"></span>
-                            </div>
-                            <div className="card relevant-comment ms-2 p-1">
-                                <span className='ms-2 fw-bold'>Emir Karataş</span>
-                                <div className='ms-2'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Culpa perferendis nemo sunt aliquid voluptate cum qui, natus quisquam laborum ad?</div>
-                            </div>
-                        </div>
-                        <span className='comments all-comments' onClick={showCommentModal}>Tüm yorumları görüntüle</span>
-                    </div></> :
-                    <>
-                        <div className="card-footer no-photo-footer">
-                            <div className="d-flex justify-content-between align-items-center">
-                                <span className='like-count' onClick={showLikeModal}><span className='fw-bold'>15</span> beğenme</span>
-                                <span className='comments' onClick={showCommentModal}><span className='fw-bold'>117</span> Yorum</span>
-                            </div>
-                            <div className="container post-icon-row border-top border-bottom my-2">
-                                <div className="row my-2">
-                                    {
-                                        like ? <button className="btn border-0 d-flex footer-icon col-4 justify-content-center p-1" onClick={handleLike}>
-                                            <FcLike  className='h4 me-2 mb-0 mt-1' />
-                                            <span>Beğen</span>
-                                        </button> : <button className="btn border-0 d-flex footer-icon col-4 justify-content-center p-1" onClick={handleLike}>
-                                            <AiOutlineHeart className='h4 me-2 mb-0 mt-1' />
-                                            <span>Beğen</span>
-                                        </button>
-                                    }
-                                    <button className="btn border-0 d-flex footer-icon col-4 justify-content-center p-1">
-                                        <GoComment className='h4 me-2 mt-1' />
-                                        <span>Yorum Yap</span>
-                                    </button>
-                                    <button className="btn border-0 d-flex footer-icon col-4 justify-content-center p-1">
-                                        <FiShare className='h4 me-2 mt-1' />
-                                        <span>Paylaş</span>
-                                    </button>
-                                </div>
-                            </div>
-                            <Modal title="Beğenmeler" open={isLikeModalOpen} onCancel={handleLikeCancel} footer={null} className='custom-modal' centered>
-                                <Likes post={post} />
-                            </Modal>
-                            <Modal title="Yorumlar" open={isCommentModalOpen} onCancel={handleCommentCancel} footer={null} className='custom-modal' centered>
-                                <Comments post={post} />
-                            </Modal>
-                            <div className="user-info d-flex align-items-start mb-2">
-                                <div className="avatar position-relative">
-                                    <img src="https://cdn-icons-png.flaticon.com/512/3135/3135715.png" alt="" />
-                                    <span className="status"></span>
-                                </div>
-                                <div className="card relevant-comment ms-2 p-1">
-                                    <span className='ms-2 fw-bold'>Emir Karataş</span>
-                                    <div className='ms-2'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Culpa perferendis nemo sunt aliquid voluptate cum qui, natus quisquam laborum ad?</div>
-                                </div>
-                            </div>
-                            <Form onSubmit={formik.handleSubmit} className='d-flex align-items-center comment-form'>
-                                <Form.Group className="mb-3 mt-3 w-100">
-                                    <Form.Control
-                                        name='comment'
-                                        className='form-control me-2'
-                                        as="textarea"
-                                        placeholder="Yorum Yaz"
-                                        onChange={formik.handleChange}
-                                    />
-                                </Form.Group>
-                                <button className="btn btn-outline-dark ms-2 h-100" type="submit">Gönder</button>
-                            </Form>
-                            <span className='comments all-comments' onClick={showCommentModal}>Tüm yorumları görüntüle</span>
-                        </div>
-                    </>
             }
+            <div className="card-footer no-photo-footer">
+                <div className="d-flex justify-content-between align-items-center">
+                    <span className='like-count' onClick={showLikeModal}><span className='fw-bold'>15</span> beğenme</span>
+                    <span className='comments' onClick={showCommentModal}><span className='fw-bold'>117</span> Yorum</span>
+                </div>
+                <div className="container post-icon-row border-top border-bottom my-2">
+                    <div className="row my-2">
+                        {
+                            like ? <button className="btn border-0 d-flex footer-icon col-4 justify-content-center p-1" onClick={handleLike}>
+                                <FcLike className='h4 me-2 mb-0 mt-1' />
+                                <span>Beğen</span>
+                            </button> : <button className="btn border-0 d-flex footer-icon col-4 justify-content-center p-1" onClick={handleLike}>
+                                <AiOutlineHeart className='h4 me-2 mb-0 mt-1' />
+                                <span>Beğen</span>
+                            </button>
+                        }
+                        <button className="btn border-0 d-flex footer-icon col-4 justify-content-center p-1">
+                            <GoComment className='h4 me-2 mt-1' />
+                            <span>Yorum Yap</span>
+                        </button>
+                        <button className="btn border-0 d-flex footer-icon col-4 justify-content-center p-1">
+                            <FiShare className='h4 me-2 mt-1' />
+                            <span>Paylaş</span>
+                        </button>
+                    </div>
+                </div>
+                <Modal title="Beğenmeler" open={isLikeModalOpen} onCancel={handleLikeCancel} footer={null} className='custom-modal' centered>
+                    <Likes post={post} />
+                </Modal>
+                <Modal title="Yorumlar" open={isCommentModalOpen} onCancel={handleCommentCancel} footer={null} className='custom-modal' centered>
+                    <Comments post={post}/>
+                </Modal>
+                <Form onSubmit={formik.handleSubmit} className='d-flex align-items-center comment-form'>
+                    <div className="user-info me-2">
+                        <div className="avatar position-relative">
+                            <img src="https://cdn-icons-png.flaticon.com/512/3135/3135715.png" alt="" />
+                        </div>
+                    </div>
+                    <Form.Group className="mb-3 mt-3 w-100">
+                        <Form.Control
+                            name='comment'
+                            className='form-control me-2'
+                            as="textarea"
+                            placeholder="Yorum Yaz"
+                            onChange={formik.handleChange}
+                        />
+                    </Form.Group>
+                    <button className="btn btn-outline-dark ms-2 h-100" type="submit">Gönder</button>
+                </Form>
+                <div className="user-info d-flex align-items-start mb-2">
+                    <div className="avatar position-relative">
+                        <img src="https://cdn-icons-png.flaticon.com/512/3135/3135715.png" alt="" />
+                    </div>
+                    <div className="card relevant-comment ms-2 p-1">
+                        <span className='ms-2 fw-bold'>Emir Karataş</span>
+                        <div className='ms-2'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Culpa perferendis nemo sunt aliquid voluptate cum qui, natus quisquam laborum ad?</div>
+                    </div>
+                </div>
+                <span className='comments all-comments' onClick={showCommentModal}>Tüm yorumları görüntüle</span>
+            </div>
         </div>
     )
 }
