6268c52201e09b82ecdff0dbde592857f5e9bd23
emir karatas <emirkaratassss@gmail.com>
2023-06-29 17:14:30 +0300

    Test Commit

--- src/pages/profile/Profile.js
+++ src/pages/profile/Profile.js
@@ -1,8 +1,11 @@
 import React from 'react'
+import "./profile.scss"
 
 function Profile() {
   return (
-    <div>Profile</div>
+    <div className='profile p-2'>
+      Profile
+    </div>
   )
 }
 
